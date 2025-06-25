6c10f9a37f59b999ec06e72024fc15d0e682070c
NageshKolli <125372532+NageshKolli@users.noreply.github.com>
2025-06-25 16:45:36 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+
+export default function App() {
+  return (
+    <div className="bg-white text-gray-900">
+      <Navbar />
+      <Hero />
+      <Projects />
+      <Footer />
+    </div>
+  );
+}
